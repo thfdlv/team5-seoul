713b6d1d0540d2a26f7291468f4f88008415b851
thfdlv <223carpediem@naver.com>
2025-04-03 15:36:27 +0900

    fix: deploy.sh tomcat경로수정 before.sh 실행 권한 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "[Deploy] Tomcat 재시작 및 WAR 복사"
 
 # Tomcat 경로 예시 (필요 시 수정)
-TOMCAT_PATH=/opt/tomcat/tomcat-10
+TOMCAT_PATH=/opt/tomcat/tomcat-10/webapps/
 
 cp /home/ec2-user/app/project1.war $TOMCAT_PATH/webapps/
 
