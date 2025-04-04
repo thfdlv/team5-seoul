f28249257d6da35647fb4c9ac5c904777759c6de
thfdlv <223carpediem@naver.com>
2025-04-04 15:01:49 +0900

    수정

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "[🚀 BEFORE INSTALL] 이전 배포파일 제거 중..."
+echo "[🚧 BEFORE INSTALL] 이전 배포 파일 제거 중..."
 rm -rf /opt/tomcat/tomcat-10/webapps/project1*
-rm -rf /home/ec2-user/scripts/*
+rm -rf /home/ec2-user/app/scripts/*
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,10 @@ TOMCAT_HOME=/opt/tomcat/tomcat-10
 # 실행 중이면 종료
 PID=$(ps -ef | grep $TOMCAT_HOME | grep -v grep | awk '{print $2}')
 if [ -n "$PID" ]; then
-  echo "⚠️ Tomcat 프로세스 종료 중..."
+  echo "[🛑] Tomcat 프로세스 종료 중..."
   kill -9 $PID
 fi
 
 # 재시작
-echo "🔁 Tomcat 재시작 중..."
+echo "[🔁] Tomcat 재시작 중..."
 $TOMCAT_HOME/bin/startup.sh
