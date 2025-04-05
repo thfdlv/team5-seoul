ea74e57feae032e9d0069e272d02697407b23ef8
thfdlv <223carpediem@naver.com>
2025-04-05 09:56:23 +0900

    수정

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -7,12 +7,12 @@ WAR_DIR="/opt/tomcat/tomcat-10/webapps/project1"
 
 if [ -f "$WAR_FILE" ]; then
   echo "👉 WAR 파일 삭제: $WAR_FILE"
-  sudo rm -f "$WAR_FILE"
+  rm -f "$WAR_FILE"
 fi
 
 if [ -d "$WAR_DIR" ]; then
   echo "👉 explode 디렉토리 삭제: $WAR_DIR"
-  sudo rm -rf "$WAR_DIR"
+  rm -rf "$WAR_DIR"
 fi
 
 echo "[BeforeInstall] 정리 완료"
