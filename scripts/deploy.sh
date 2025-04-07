90e65f6339771cf81adb8df12f4c6b660b9f7a77
thfdlv <223carpediem@naver.com>
2025-04-07 13:11:40 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,16 @@
 
 echo "[🚀 DEPLOY] WAR 파일 실행 준비 중..."
 
-# 기존 Tomcat 종료
 echo "[🛑] Tomcat 프로세스 종료 중..."
 sudo pkill -f 'org.apache.catalina.startup.Bootstrap'
 
-# WAR 복사
-echo "[📦] WAR 복사"
+echo "[🧹] 기존 WAR 및 디렉터리 삭제 중..."
+sudo rm -f /opt/tomcat/tomcat-10/webapps/project1.war
+sudo rm -rf /opt/tomcat/tomcat-10/webapps/project1
+
+echo "[📦] 새 WAR 복사"
 sudo cp /home/ec2-user/app/project1.war /opt/tomcat/tomcat-10/webapps/
 
-# Tomcat 재시작
 echo "[🔁] Tomcat 재시작 중..."
 sudo /opt/tomcat/tomcat-10/bin/startup.sh
 
